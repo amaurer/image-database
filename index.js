0cd145640f3a429c7f14c075ee54f8266b97d6ce
Andrew Maurer <andrew@maurer.me>
2015-07-11 11:44:35 -0400

    Added first stab at query

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
 
 
+var IDB = require("./lib/image-database.js")
 
-/**
-var imdb = require("image-database")
-var catalog = imdb.read("/test/");
-var otherstuff = imdb.read("/stuff/");
-*/
-
-module.id = "image-database"
-
-function ImageDatabase(imageRootPath, done){
-	this.types = ["jpg","png","gif"]
-	this.rootDir = imageRootPath || __dirname
-	this.collection = {}
-
-	var dirReader = require("lib/directory-reader")
-	var collectionBuilder = require("lib/collection-builder")
-
-	var test = dirReader.get({ rootDir : this.rootDir }, function(files){
-		collection = collectionBuilder.build(files, done)
-	})
-}
-
-ImageDatabase.prototype.findCollection = function(collectionFilter) {
-	collectionFilter = "/group/section/name"
-	return collectionBuilder.find(collectionFilter)
-};
-
-
-
-
-
-module.exports = ImageDatabase
+var idb = new IDB("./tests", function(e, collection){
+	console.log("doner")
+	console.log(collection.tests)
+})
